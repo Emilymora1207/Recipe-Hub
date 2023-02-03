7543de1e84b3ff2c516202a8b7a2898f642da71e
mohamad ghaznawy <mdawoodghaznawy@yahoo.com.com>
2023-02-02 18:44:26 -0800

    file added

--- Recipe.js
+++ Recipe.js
@@ -1,11 +1,6 @@
-var result = document.querySelector("#result");
-var searchBtn = document.querySelector("#search-btn");
-var recipe = document.querySelector(".recipe");
-var ingredItem = document.querySelector(".ingredients");
+// var url = "https://www.themealdb.com/api/json/v1/1/search.php?f=";
 
-var url = "https://www.themealdb.com/api/json/v1/1/search.php?f=";
-
-var userInput = document.getElementById("user-input").value;
+// var userInput = document.getElementById("user-input").value;
 // fetch(url + "a")
 //   .then((Response) => Response.json())
 //   .then((data) => {
@@ -22,6 +17,10 @@ var userInput = document.getElementById("user-input").value;
 //     ing.appendChild(ingredientsItem);
 //   }
 // }
+var result = document.querySelector("#result");
+var searchBtn = document.querySelector("#search-btn");
+var recipe = document.querySelector(".recipe");
+var ingredItem = document.querySelector(".ingredients");
 
 const options = {
   method: "GET",
@@ -31,22 +30,24 @@ const options = {
   },
 };
 
-fetch(
-  "https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup",
-  options
-)
-  .then((response) => response.json())
-  .then((response) => {
-    console.log(response);
-
-    for (var i = 0; i < response.results.length; i++) {
-      var myMeal = response.results[i];
-      var ingredientsItem = document.createElement("li");
-      ingredientsItem.textContent = myMeal.display;
-      console.log(myMeal.display);
-      ingredItem.appendChild(ingredientsItem);
-    }
-  });
 searchBtn.addEventListener("click", function (e) {
   e.preventDefault();
+  var userInput = document.querySelector("#user-input").value;
+  console.log(userInput);
+  fetch(
+    "https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+
+      for (var i = 0; i < response.results.length; i++) {
+        var myMeal = response.results[i];
+        var ingredientsItem = document.createElement("li");
+        ingredientsItem.textContent = myMeal.display;
+        console.log(myMeal.display);
+        ingredItem.appendChild(ingredientsItem);
+      }
+    });
 });
