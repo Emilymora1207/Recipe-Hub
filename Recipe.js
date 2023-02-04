1f8944248e2443e9d15112bdf90463e9a07c152e
mohamad ghaznawy <mdawoodghaznawy@yahoo.com.com>
2023-02-04 01:09:42 -0800

    added code to recipe.js

--- Recipe.js
+++ Recipe.js
@@ -1,53 +1,49 @@
-// var url = "https://www.themealdb.com/api/json/v1/1/search.php?f=";
-
-// var userInput = document.getElementById("user-input").value;
-// fetch(url + "a")
-//   .then((Response) => Response.json())
-//   .then((data) => {
-//     var myMeal = data.meals[0];
-//     console.log(myMeal);
-//     console.log(Response);
-//     getRecipe(myMeal);
-//   });
-// function getRecipe(myMeal) {
-//   for (var i = 0; i < 20; i++) {
-//     var ingredientsItem = document.createElement("li");
-//     ingredientsItem.textContent = myMeal.strIngredient[i];
-//     console.log(myMeal.strIngredient[i]);
-//     ing.appendChild(ingredientsItem);
-//   }
-// }
 var result = document.querySelector("#result");
 var searchBtn = document.querySelector("#search-btn");
 var recipe = document.querySelector(".recipe");
-var ingredItem = document.querySelector(".ingredients");
-
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "208fb9c73emsh8b7abaaea8ec0ecp1e4d8ajsn1a0b17075460",
-    "X-RapidAPI-Host": "tasty.p.rapidapi.com",
-  },
-};
+var ingredList = document.querySelector(".ingredients");
 
 searchBtn.addEventListener("click", function (e) {
   e.preventDefault();
   var userInput = document.querySelector("#user-input").value;
-  console.log(userInput);
-  fetch(
-    "https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup",
-    options
-  )
+
+  var modifiedInput = userInput.replaceAll("%20");
+
+  console.log(userInput, modifiedInput);
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + modifiedInput)
     .then((response) => response.json())
     .then((response) => {
       console.log(response);
 
-      for (var i = 0; i < response.results.length; i++) {
-        var myMeal = response.results[i];
-        var ingredientsItem = document.createElement("li");
-        ingredientsItem.textContent = myMeal.display;
-        console.log(myMeal.display);
-        ingredItem.appendChild(ingredientsItem);
-      }
+      displayRecipe(response);
     });
 });
+function displayRecipe(response) {
+  for (var k = 0; k < response.meals.length; k++) {
+    var myMeal = response.meals[k];
+    var recipeItem = document.createElement("li");
+    ingredList.appendChild(recipeItem);
+    var itemTitle = document.createElement("h2");
+    itemTitle.textContent = myMeal.strMeal;
+    var itemImg = document.createElement("img");
+    itemImg.setAttribute("src", myMeal.strMealThumb);
+    recipeItem.appendChild(itemTitle);
+
+    recipeItem.appendChild(itemImg);
+    var listList = document.createElement("ul");
+
+    recipeItem.appendChild(listList);
+    for (var l = 0; l < 20; l++) {
+      if (myMeal["strIngredient" + l] === "") {
+        break;
+      }
+      var ingridMeasure = document.createElement("li");
+      listList.appendChild(ingridMeasure);
+      ingridMeasure.textContent =
+        myMeal["strIngredient" + l] + ":" + myMeal["strMeasure" + l];
+    }
+    var instructionsItem = document.createElement("p");
+    recipeItem.appendChild(instructionsItem);
+    instructionsItem.textContent = myMeal.strInstructions;
+  }
+}
